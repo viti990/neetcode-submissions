2d1c9d525052c8fd02d13f382f4534528c570916
viti990 <95943868+viti990@users.noreply.github.com>
2026-07-27 17:31:47 -0300

    Add: minimum-stack - submission-4

---
+++ Data Structures & Algorithms/minimum-stack/submission-4.cpp
@@ -0,0 +1,42 @@
+class MinStack {
+public:
+    MinStack() {
+        pool.reserve(capacity);
+        minVector.reserve(capacity);
+    }
+    
+    void push(int val) {
+        pool.push_back(val);
+        if(val<min)
+        {
+            min=val;
+        }
+        minVector.push_back(min);
+    }
+    
+    void pop() {
+        pool.pop_back();
+        minVector.pop_back();
+        if(minVector.size()!=0)
+        {
+            min = minVector.back();
+        }
+        else
+        {
+            min = std::numeric_limits<int>::max();
+        }
+    }
+    
+    int top() {
+        return pool.back();
+    }
+    
+    int getMin() {
+        return minVector.back();
+    }
+private:
+    std::vector<int> pool;
+    std::vector<int> minVector;
+    int min{std::numeric_limits<int>::max()};
+    int capacity{10};
+};
