6b38900a0037af5caf484b26b98969f87020aa90
viti990 <95943868+viti990@users.noreply.github.com>
2026-08-09 21:22:04 -0300

    Add: longest-substring-without-duplicates - submission-12

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-12.cpp
@@ -0,0 +1,33 @@
+class Solution 
+{
+    public:
+        int lengthOfLongestSubstring(string s) 
+        {
+            char* ptr1 = &s[0];
+            char* ptr2 = &s[0];
+            char* last_addr = &s.back();
+            std::unordered_set<char> mySet;
+            int max{}, count{};
+        
+            while(ptr2 != last_addr+1)
+            {
+                while(ptr1 !=ptr2 && mySet.contains(*ptr2))
+                {
+                    mySet.erase(*ptr1);
+                    ptr1 += 1; 
+                }
+                if(!mySet.contains(*ptr2))
+                {
+                    mySet.insert(*ptr2);
+                    ptr2 += 1;
+                }
+                count = ptr2 - ptr1;
+                if(count > max)
+                {
+                    max = count;
+                }
+                //std::cout << count << ' ';
+            }
+            return max;
+        }
+};
